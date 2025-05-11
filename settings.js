1715981b68be5bfb59da5a259c40670d8921558b
chatuwa <matheebaby5251@gmail.com>
2025-05-11 17:57:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-
-𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yy5WFbRL#NR-X1shrqXUx_IsMF0mxCtF7FWlpH5VMEuG6vN3XUDY: process.env.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yy5WFbRL#NR-X1shrqXUx_IsMF0mxCtF7FWlpH5VMEuG6vN3XUDY === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=32pQlDLS#EM2MbhJvScMPazpSfBf3MAlkrFxUzwuv-5rKwhdhFqI' : process.env.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=yy5WFbRL#NR-X1shrqXUx_IsMF0mxCtF7FWlpH5VMEuG6vN3XUDY,
+𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bu5lTLIS#PS_y6t06EefNKhTaw4KM4HIG2JhsUIm5LU42h-I4RiY: process.env.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bu5lTLIS#PS_y6t06EefNKhTaw4KM4HIG2JhsUIm5LU42h-I4RiY === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=32pQlDLS#EM2MbhJvScMPazpSfBf3MAlkrFxUzwuv-5rKwhdhFqI' : process.env.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bu5lTLIS#PS_y6t06EefNKhTaw4KM4HIG2JhsUIm5LU42h-I4RiY,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
